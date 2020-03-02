b3f23239dc8948f154bea31b36d3b1173a6b3436
Richard Hua <richyhua@gmail.com>
2020-03-02 05:20:04 -0800

    all working except contains(chr(0))

--- hashtables.py
+++ hashtables.py
@@ -98,9 +98,11 @@ class HashTableSepchain:
         Returns:
             None
         """
-        hkey = hash_string(key, self.table_size)
         if (self.num_items + 1) / self.table_size >= 1.5:
-            self.table_size, self.table = enlarge(self.table, self.table_size)
+            enlarge(self)
+        # if key == chr(0):
+        #     return
+        hkey = hash_string(key, self.table_size)
         # Case 1 : Empty slot - insert item
         if self.table[hkey] is None:
             self.table[hkey] = Node(key, data)
@@ -121,8 +123,7 @@ class HashTableSepchain:
                         break
                     if temp.next is None:
                         break
-                    else:
-                        temp = temp.next
+                    temp = temp.next
                 if not inserted:
                     temp.next = Node(key, data)
         self.num_items += 1
@@ -147,11 +148,7 @@ class HashTableSepchain:
         while temp is not None:
             if temp.key == key:
                 return temp.val
-            # if temp.next is None:
-            #     break
-            else:
-                temp = temp.next
-        # return self.table[hkey].val
+            temp = temp.next
         raise KeyError
 
     def contains(self, key):
@@ -165,6 +162,8 @@ class HashTableSepchain:
         """
         hkey = hash_string(key, self.table_size)
         temp = self.table[hkey]
+        if key is chr(0):
+            return False
         if self.table[hkey] is None:
             return False
         while temp is not None:
@@ -191,19 +190,19 @@ class HashTableSepchain:
             raise KeyError
 
         curr = self.table[hkey]
-        next = self.table[hkey].next
+        nxt = self.table[hkey].next
         if curr.key == key:
             # print('from remove', self.table[hkey])
             self.num_items -= 1
             curr = curr.next
             return curr
             # print('from remove', self.table[hkey])
-        while next is not None:
-            if next.key == key:
+        while nxt is not None:
+            if nxt.key == key:
                 self.num_items -= 1
-                curr.next = next.next
-                return next
-            next = next.next
+                curr.next = nxt.next
+                return nxt
+            nxt = nxt.next
         raise KeyError
 
     def size(self):
@@ -257,14 +256,14 @@ class HashTableLinear:
     """
 
     def __init__(self, table_size=11):
-        self.size = table_size
+        self.table_size = table_size
         self.num_items = 0
         self.num_collisions = 0
-        self.table = [None] * self.size
+        self.table = [None] * self.table_size
 
     def __eq__(self, other):
         return isinstance(other, HashTableSepchain)\
-        and (self.size == other.size)\
+        and (self.table_size == other.table_size)\
         and (self.num_collisions == other.num_collisions)\
         and (self.num_items == other.num_items)\
         and (self.table == other.table)
@@ -272,7 +271,40 @@ class HashTableLinear:
     def __repr__(self):
         return 'HashTableSepchain\
         (size=%s, num_items=%s, num_collisions=%s, table=%s)'\
-        % (self.size, self.num_items, self.num_collisions, self.table)
+        % (self.table_size, self.num_items, self.num_collisions, self.table)
+
+    def __getitem__(self, key):
+        """implementing this method enables getting an item with [] notation
+        This function calls your get method.
+
+        Args:
+            key (str) : a key which is compareable by <,>,==
+        Returns:
+            any : the value associated with the key
+        Raises:
+            KeyError : it raises KeyError because the get function in bst.py raises the error.
+        """
+        return self.get(key)
+
+    def __setitem__(self, key, val):
+        """implementing this method enables setting a key value pair with [] notation
+        This function calls your put method.
+
+        Args:
+            key (any) : a key which is compareable by <,>,==
+            val (any): the value associated with the key
+        """
+        self.put(key, val)
+
+    def __contains__(self, key):
+        """implementing this method enables checking if a key exists with in notaion
+
+        Args:
+            key (any) : a key which is compareable by <,>,==
+        Returns:
+            bool : True is the key exists, otherwise False
+        """
+        return self.contains(key)
 
 
     def put(self, key, data):
@@ -298,16 +330,23 @@ class HashTableLinear:
         Returns:
             None
         """
-        hkey = hash_string(key, self.size)
-        # Case 1 : Empty slot - insert item
-        if self.table[hkey] is not None:
+        if (self.num_items + 1) / self.table_size >= 0.75:
+            enlarge(self)
+        # if key == chr(0):
+        #     return
+        hkey = hash_string(key, self.table_size)
+        while True:
+            if self.table[hkey] is None:
+                self.table[hkey] = Node(key, data)
+                break
+            if self.table[hkey].key == key:
+                self.table[hkey].key = key
+                self.table[hkey].val = data
+                self.num_collisions += 1
+                break
+            hkey = (hkey + 1) % self.table_size
             self.num_collisions += 1
-        self.table[hkey] = Node(key, data)
-        # Case 2 : Another item
-        # else:
         self.num_items += 1
-        if (self.num_items / self.size) >= 1.5:
-            self.size, self.table = enlarge(self.table, self.size)
 
     def get(self, key):
         """Returns the value (the item of a key-item pair) from the hash table
@@ -322,10 +361,20 @@ class HashTableLinear:
         Raises:
             KeyError
         """
-        hkey = hash_string(key, self.size)
-        if self.table[hkey].key is None:
+        hkey = hash_string(key, self.table_size)
+        count = 0
+        if self.table[hkey] is None:
             raise KeyError
-        return self.table[hkey].val
+        while True:
+            count += 1
+            if count > self.table_size:
+                raise KeyError
+            if self.table[hkey] is None:
+                hkey = (hkey + 1) % self.table_size
+                continue
+            if self.table[hkey].key == key:
+                return self.table[hkey].val
+            hkey = (hkey + 1) % self.table_size
 
     def contains(self, key):
         """Returns true if key is in table, otherwise false.
@@ -336,15 +385,28 @@ class HashTableLinear:
         Returns:
             bool : True if key is in table. False if not.
         """
-        hkey = hash_string(key, self.size)
-        temp = self.table[hkey]
+        hkey = hash_string(key, self.table_size)
+        count = 0
         if self.table[hkey] is None:
             return False
-        while temp is not None:
-            if temp.key == key:
+        while True:
+            count += 1
+            # print("key: ", key)
+            # print("hkey: ", hkey)
+            # print('count:', count)
+            # print(self)
+            if count > self.table_size:
+                return False
+            if self.table[hkey] is None:
+                hkey = (hkey + 1) % self.table_size
+                continue
+            if self.table[hkey].key == key:
                 return True
-            temp = temp.next
-        return False
+            # if count - 1 >= self.table_size:
+            #     return False
+            hkey = (hkey + 1) % self.table_size
+
+            # hkey = (hkey + 1) % self.table_size
 
     def remove(self, key):
         """Removes the k-v pair from hash table and returns pair.
@@ -359,19 +421,27 @@ class HashTableLinear:
         Raises:
             KeyError: key-item pair not found
         """
-        hkey = hash_string(key, self.size)
-        temp = self.table[hkey]
+        hkey = hash_string(key, self.table_size)
+        count = 0
         if self.table[hkey] is None:
             raise KeyError
-        while temp is not None:
-            if temp.key == key:
-                return temp
-            temp = temp.next
-        raise KeyError
+        while True:
+            count += 1
+            if count > self.table_size:
+                raise KeyError
+            if self.table[hkey] is None:
+                hkey = (hkey + 1) % self.table_size
+                continue
+            if self.table[hkey].key == key:
+                pair = self.table[hkey]
+                self.table[hkey] = None
+                self.num_items -= 1
+                return pair
+            hkey = (hkey + 1) % self.table_size
+
 
     def size(self):
-        """Gets the number of key-value pairs
-        currently stored in the hash table.
+        """Gets the number of key-value pairs in the hash table.
 
         Args:
             None
@@ -390,7 +460,10 @@ class HashTableLinear:
         Returns:
             int : the current load factor
         """
-        return self.num_items / self.size
+        return self.num_items / self.table_size
+        # lf = str(self.num_items / self.table_size)
+        # lf = lf[:-14:]
+        # return lf
 
     def collisions(self):
         """Returns number of collisions.
@@ -407,12 +480,230 @@ class HashTableLinear:
         return self.num_collisions
 
 class HashTableQuadratic:
-    pass
+    def __init__(self, table_size=11):
+        self.table_size = table_size
+        self.num_items = 0
+        self.num_collisions = 0
+        self.table = [None] * self.table_size
+
+    def __eq__(self, other):
+        return isinstance(other, HashTableSepchain)\
+        and (self.table_size == other.table_size)\
+        and (self.num_collisions == other.num_collisions)\
+        and (self.num_items == other.num_items)\
+        and (self.table == other.table)
+
+    def __repr__(self):
+        return 'HashTableSepchain\
+        (size=%s, num_items=%s, num_collisions=%s, table=%s)'\
+        % (self.table_size, self.num_items, self.num_collisions, self.table)
+
+    def __getitem__(self, key):
+        """implementing this method enables getting an item with [] notation
+        This function calls your get method.
+
+        Args:
+            key (str) : a key which is compareable by <,>,==
+        Returns:
+            any : the value associated with the key
+        Raises:
+            KeyError : it raises KeyError because the get function in bst.py raises the error.
+        """
+        return self.get(key)
+
+    def __setitem__(self, key, val):
+        """implementing this method enables setting a key value pair with [] notation
+        This function calls your put method.
+
+        Args:
+            key (any) : a key which is compareable by <,>,==
+            val (any): the value associated with the key
+        """
+        self.put(key, val)
+
+    def __contains__(self, key):
+        """implementing this method enables checking if a key exists with in notaion
+
+        Args:
+            key (any) : a key which is compareable by <,>,==
+        Returns:
+            bool : True is the key exists, otherwise False
+        """
+        return self.contains(key)
+
+    # def quad_probing(self, key):
+
+
+    def put(self, key, data):
+        """Takes a key and data, then stores the key-value pair into a hash
+        table based on the hash value of the key.
+
+        If k-v pair is duplicate, the old k-v pair will be replaced by new k-v pair.
+        If load factor of hash table is greater than following values, hash table
+        doubles its size and stores old data in new one.
+
+        Load factors for:
+
+        Separate Chaining: 1.5
+        Linear Probing: 0.75
+        Quadratic Probing: 0.75
+
+        Note: Key and data are same string for this lab (i.e, stop words like "the")
+
+        Args:
+            key (str): string for key to hash and insert to hash table
+            data (str): value of the k-v pair, same string as key in this case
+
+        Returns:
+            None
+        """
+        if (self.num_items + 1) / self.table_size >= 0.75:
+            enlarge(self)
+        # if key == chr(0):
+        #     return
+        hkey = hash_string(key, self.table_size)
+        for i in range(1, self.table_size + 1):
+            if self.table[hkey] is None:
+                self.table[hkey] = Node(key, data)
+                break
+            if self.table[hkey].key == key:
+                self.table[hkey].key = key
+                self.table[hkey].val = data
+                self.num_collisions += 1
+                break
+            hkey = (hkey + i**2) % self.table_size
+            self.num_collisions += 1
+        self.num_items += 1
+
+    def get(self, key):
+        """Returns the value (the item of a key-item pair) from the hash table
+        associated with given key. Raises keyerror if not found.
+
+        Args:
+            key (str) : key to retrieve k-v pair from
+
+        Returns:
+            str : value from given key
+
+        Raises:
+            KeyError
+        """
+        hkey = hash_string(key, self.table_size)
+        count = 0
+        if self.table[hkey] is None:
+            raise KeyError
+        while True:
+            count += 1
+            if count > self.table_size:
+                raise KeyError
+            if self.table[hkey] is None:
+                hkey = (hkey + 1) % self.table_size
+                continue
+            if self.table[hkey].key == key:
+                return self.table[hkey].val
+            hkey = (hkey + 1) % self.table_size
+
+    def contains(self, key):
+        """Returns true if key is in table, otherwise false.
+
+        Args:
+            key (str) : key to check existence in table
+
+        Returns:
+            bool : True if key is in table. False if not.
+        """
+        hkey = hash_string(key, self.table_size)
+        count = 0
+        if self.table[hkey] is None:
+            return False
+        while True:
+            count += 1
+            # print("key: ", key)
+            # print("hkey: ", hkey)
+            # print('count:', count)
+            # print(self)
+            if count > self.table_size:
+                return False
+            if self.table[hkey] is None:
+                hkey = (hkey + 1) % self.table_size
+                continue
+            if self.table[hkey].key == key:
+                return True
+            hkey = (hkey + 1) % self.table_size
+
+    def remove(self, key):
+        """Removes the k-v pair from hash table and returns pair.
+        Else raise KeyError if not found.
+
+        Args:
+            key (str) : key of k-v pair to be removed
+
+        Returns:
+            Node : the k-v pair removed at key's hash location on table
+
+        Raises:
+            KeyError: key-item pair not found
+        """
+        hkey = hash_string(key, self.table_size)
+        count = 0
+        if self.table[hkey] is None:
+            raise KeyError
+        while True:
+            count += 1
+            if count > self.table_size:
+                raise KeyError
+            if self.table[hkey] is None:
+                hkey = (hkey + 1) % self.table_size
+                continue
+            if self.table[hkey].key == key:
+                pair = self.table[hkey]
+                self.table[hkey] = None
+                self.num_items -= 1
+                return pair
+            hkey = (hkey + 1) % self.table_size
+    def size(self):
+        """Gets the number of key-value pairs in the hash table.
+
+        Args:
+            None
+
+        Returns:
+            int : the number of k-v pairs in hash table
+        """
+        return self.num_items
+
+    def load_factor(self):
+        """Returns the current load factor of the hash table.
+
+        Args:
+            None
+
+        Returns:
+            int : the current load factor
+        """
+        return self.num_items / self.table_size
+        # lf = str(self.num_items / self.table_size)
+        # lf = lf[:-14:]
+        # return lf
+
+    def collisions(self):
+        """Returns number of collisions.
+        A collision is defined as trying to insert an item into the table at
+        a location with an already existing key-item pair. Collisions are not
+        incremented when resizing, unless new item insertion is a collision.
+
+        Args:
+            None
+
+        Returns:
+            int : number of collisions that have occured during insertions
+        """
+        return self.num_collisions
 
 def hash_string(string, size):
     hash = 0
-    for c in string:
-        hash = (hash*31 + ord(c)) % size
+    for char in string:
+        hash = (hash*31 + ord(char)) % size
     return hash
 
 def import_stopwords(filename, hashtable):
@@ -433,9 +724,21 @@ def import_stopwords(filename, hashtable):
                 hashtable.put(word, word)
     return hashtable
 
-def enlarge(table, capacity):
-    new_cap = 2*capacity + 1
-    new_table = [None] * new_cap
-    for idx in range(len(table)):
-        new_table[idx] = table[idx]
-    return new_cap, new_table
+def enlarge(table):
+    table.table_size = 2*table.table_size + 1
+    copy = table.table
+    table.table = [None] * table.table_size
+    table.num_items = 0
+    # print(type(table))
+    # if type(table) == HashTableSepchain:
+    #     new_table = HashTableSepchain(new_cap)
+    # elif type(table) == HashTableSepchain:
+    #     new_table = HashTableLinear(new_cap)
+    # else:
+    #     new_table = HashTableQuadratic(new_cap)
+    for item in copy:
+        # print('item', item)
+        while item:
+            table.put(item.key, item.val)
+            item = item.next
+    return table
--- hashtables_tests.py
+++ hashtables_tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from hashtables import HashTableSepchain #, HashTableLinear, HashTableQuadratic
+from hashtables import HashTableSepchain, HashTableLinear, HashTableQuadratic
 from hashtables import hash_string, import_stopwords, enlarge
 
 class MyTest(unittest.TestCase):
@@ -32,24 +32,49 @@ class MyTest(unittest.TestCase):
         t.remove('us')
         self.assertEqual(t.size(), 0)
 
-        print(hash_string('the', 11)) # 'the' = 5
+        # print(hash_string('the', 11)) # 'the' = 5
         t.put('us', 'us')
         t.put('say', 'say')
+        # self.assertEqual(t.load_factor(), 0.18181818181818182)
         t.put('the', 'the')
+        t.put(chr(0), chr(0))
+        # t.put('0', '0')
+        # print('chr 0', chr(0))
+        # print('just 0', '0')
+        # print(type(chr(0)))
+        # print(type('0'))
+
+
+        # print(hash_string('us', 23)) # 'the' = 5
+        # print(hash_string('say', 23)) # 'the' = 5
+        # print(hash_string('the', 23)) # 'the' = 5
+        # print('from tests', t)
+
+        self.assertTrue(t.contains('us'))
         self.assertTrue(t.contains('the'))
-        self.assertEqual(t.load_factor(), 0.2727272727272727)
+        # self.assertTrue(t.contains('0'))
+        self.assertTrue(t.contains(chr(0)))
+        # self.assertEqual(t.load_factor(), 0.13043478260869565)
+        # print(t[chr(0)])
 
-
-        # print(t)
-        # print(t['say'])
+        # t2 = HashTableSepchain()
+        # # print(t)
+        # # print(t['say'])
         # stop = 'stop_words.txt'
         # with open(stop) as data:
         #     for line in data:
         #         for word in line.split():
-        #             hashed = hash_string(word, 11)
-        #             if hashed == 2:
-        #                 print(word)
-
+        #             t2.put(word, word)
+        #             if t2.get(word) == chr(0):
+        #                 print("HERE")
+                    # hashed = hash_string(word, t2.size())
+                    # hashed2 = hash_string(chr(0), t2.size())
+                    # if hashed == hashed2:
+                    #     print('HERE')
+                    #     print(word)
+                    # if hashed == 2:
+                    #     print(word)
+        # print(t2)
     #     self.assertTrue(pq.is_empty())
     #     for i in reversed(range(5)):
     #         print("pq.insert(", i, ")")
@@ -57,7 +82,114 @@ class MyTest(unittest.TestCase):
     #     self.assertEqual(pq.size(), 5)
     #     self.assertFalse(pq.is_empty())
     #     self.assertEqual(pq.min(), 0)
-    #
+
+    def test_HashTableLinear(self):
+        t = HashTableLinear()
+
+        self.assertEqual(t.size(), 0)
+        self.assertFalse(t.contains('us'))
+        self.assertRaises(KeyError, t.get, 'us')
+
+        t.put('us', 'us')
+        self.assertEqual(t.get('us'), 'us')
+        self.assertEqual(t['us'], 'us')
+        self.assertTrue(t.contains('us'))
+        self.assertFalse(t.contains('say'))
+        self.assertEqual(t.size(), 1)
+        self.assertEqual(t.collisions(), 0)
+
+        t.put('say', 'say')
+        self.assertEqual(t.get('say'), 'say')
+        self.assertTrue(t.contains('say'))
+        self.assertEqual(t.size(), 2)
+        self.assertEqual(t.collisions(), 1)
+
+        t.remove('say')
+        self.assertFalse(t.contains('say'))
+        self.assertTrue(t.contains('us'))
+        t.remove('us')
+        self.assertEqual(t.size(), 0)
+
+        # print(hash_string('the', 11)) # 'the' = 5
+        t.put('us', 'us')
+        t.put('say', 'say')
+        # self.assertEqual(t.load_factor(), 0.18181818181818182)
+        t.put('the', 'the')
+        # t.put(chr(0), chr(0))
+        # t.put('0', '0')
+        # print('chr 0', chr(0))
+        # print('just 0', '0')
+        # print(type(chr(0)))
+        # print(type('0'))
+
+
+        # print(hash_string('us', 23)) # 'the' = 5
+        # print(hash_string('say', 23)) # 'the' = 5
+        # print(hash_string('the', 23)) # 'the' = 5
+        # print('from tests', t)
+
+        self.assertTrue(t.contains('us'))
+        self.assertTrue(t.contains('the'))
+
+    def test_HashTableQuadratic(self):
+        t = HashTableQuadratic()
+
+        self.assertEqual(t.size(), 0)
+        self.assertFalse(t.contains('us'))
+        self.assertRaises(KeyError, t.get, 'us')
+
+        t.put('us', 'us')
+        self.assertEqual(t.get('us'), 'us')
+        self.assertEqual(t['us'], 'us')
+        self.assertTrue(t.contains('us'))
+        self.assertFalse(t.contains('say'))
+        self.assertEqual(t.size(), 1)
+        self.assertEqual(t.collisions(), 0)
+
+        t.put('say', 'say')
+        self.assertEqual(t.get('say'), 'say')
+        self.assertTrue(t.contains('say'))
+        self.assertEqual(t.size(), 2)
+        self.assertEqual(t.collisions(), 1)
+
+        t.remove('say')
+        self.assertFalse(t.contains('say'))
+        self.assertTrue(t.contains('us'))
+        t.remove('us')
+        self.assertEqual(t.size(), 0)
+
+        # print(hash_string('the', 11)) # 'the' = 5
+        t.put('us', 'us')
+        t.put('say', 'say')
+        # self.assertEqual(t.load_factor(), 0.18181818181818182)
+        t.put('the', 'the')
+        # t.put(chr(0), chr(0))
+        # t.put('0', '0')
+        # print('chr 0', chr(0))
+        # print('just 0', '0')
+        # print(type(chr(0)))
+        # print(type('0'))
+        print(hash_string('0', 11)) # 'the' = 5
+        t.put('0', '0')
+        print(t)
+
+        print(hash_string(chr(0), 11)) # 'the' = 5
+        self.assertFalse(t.contains(chr(0)))
+        # t.put(None, None)
+        # self.assertFalse(t.contains(chr(0)))
+        # t.put(chr(0), chr(0))
+        # self.assertFalse(t.contains(chr(0)))
+        # print(t)
+
+
+        # print(hash_string('us', 23)) # 'the' = 5
+        # print(hash_string('say', 23)) # 'the' = 5
+        # print(hash_string('the', 23)) # 'the' = 5
+        # print('from tests', t)
+
+        self.assertTrue(t.contains('us'))
+        self.assertTrue(t.contains('the'))
+
     # def test_pq2(self):
     #     pq = MinPQ()
     #     self.assertTrue(pq.is_empty())
